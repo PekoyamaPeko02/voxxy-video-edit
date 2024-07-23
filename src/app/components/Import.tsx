6acc583b4aa0fed0ed0446c3bf1c2253a08c0449
tanatanJ <jakelong2031@hotmail.com>
2024-07-23 21:53:07 +0700

    adjust size css

--- src/app/components/Import.tsx
+++ src/app/components/Import.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { IconSearch} from '@tabler/icons-react';
 
 const Import: React.FC = () => {
   return (
     <div>
       <h2>Import Tool</h2>
       {/* Add your import tool UI here */}
-      <p>This is the Import tool asfomopcontentasfmjasiopfajmipo.</p>
+      <p>This is the Import tool </p>
+      <IconSearch/>
     </div>
   );
 };
