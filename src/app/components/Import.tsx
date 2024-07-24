fb801f373b6db1398fb96096008d0d91023c96f1
tanatanJ <jakelong2031@hotmail.com>
2024-07-24 18:59:35 +0700

    Update Import.tsx

--- src/app/components/Import.tsx
+++ src/app/components/Import.tsx
@@ -5,7 +5,6 @@ const Import: React.FC = () => {
   return (
     <div>
       <h2>Import Tool</h2>
-      {/* Add your import tool UI here */}
       <p>This is the Import tool </p>
       <IconSearch/>
     </div>
